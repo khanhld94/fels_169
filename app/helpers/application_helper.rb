43bb3ad9d3ad5452ea97bb2cf72c8904097cab70
dhung133 <dhug133@gmail.com>
2016-08-03 15:23:22 +0700

    Add word answer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,16 @@ module ApplicationHelper
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
 
-  def link_to_add_fields name, f, type
+  def link_to_add_fields name, f, association
+    new_object = f.object.send(association).build
+    id = new_object.object_id
+    fields = f.fields_for association, new_object, child_index: id do |builder|
+      render association.to_s.singularize + "_fields", f: builder
+    end
+    link_to name, "#", class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")}
+  end
+
+  def link_to_add_fields_search name, f, type
     new_object = f.object.send "build_#{type}"
     id = "new_#{type}"
     fields = f.send "#{type}_fields", new_object, child_index: id do |builder|
